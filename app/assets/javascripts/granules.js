4976d4f184c98b6e70bf8e6394f333d05e7977b9
cduma <cv.duma@gmail.com>
2018-01-30 10:52:55 -0500

    CMROS-91 part 3 - Add client side validataion in addition to back-end validation per PR feedback

--- app/assets/javascripts/granules.js
+++ app/assets/javascripts/granules.js
@@ -4,4 +4,27 @@
 //= require jquery-ui-1.8.20.custom.min
 //= require jquery-ui-sliderAccess
 //= require jquery-ui-timepicker-addon
+//= require sweetalert.min
 //= require results
+
+$(function() {
+    $('#granules-search-form').on("submit", function (e) {
+        collectionConceptId = $('#parentIdentifier').val();
+        shortName = $('#shortName').val();
+        uniqueId = $('#uid').val();
+        // search must have at least one of the collection concept id, collection short name of granule unique id
+        if (collectionConceptId === '' && shortName === '' && uniqueId === '') {
+            swal({
+                title: "Granules search error!",
+                text: "Granule searches require at least the Collection Concept ID or Short Name or Unique ID fields.",
+                icon: "error"
+            });
+            // consume the submit
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    });
+});
