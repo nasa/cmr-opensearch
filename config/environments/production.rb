a6aa370a679a0f8847e05274c844e45008638060
cduma <cv.duma@gmail.com>
2018-03-08 09:36:52 -0500

    CMROS-100 try fix for PROD deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-EchoOpensearch.application.configure do
+EchoOpensearch.Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
